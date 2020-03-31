8188e410b04eaaca77970c3eae23ae56f272f84f
doubix <adib.grouz@gmail.com>
2020-03-31 17:16:57 +0100

    [update] amenity model mysql schema

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 """This is the amenity class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """This is the class for Amenity
     Attributes:
-        name: input name
+        __tablename__ (str): MySQL table name to store Amenities.
+        name (sqlalchemy String): amenity name.
+        place_amenities (sqlalchemy relationship): Place-Amenity relationship.
     """
-    name = ""
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                   viewonly=False)

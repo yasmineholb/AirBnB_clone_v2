46ea78bf0f41b062b13c2c8b782d8704ccc7f083
doubix <adib.grouz@gmail.com>
2020-03-31 17:20:27 +0100

    [update] state model mysql schema

--- models/state.py
+++ models/state.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel):
     """This is the class for State
     Attributes:
-        name: input name
+        __tablename__ (str): The name of the MySQL table to store States.
+        name (sqlalchemy String): State name.
+        cities (sqlalchemy relationship): The State-City relationship.
     """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City",  backref="state", cascade="delete")

15bc7f87268f40f3573149c16bd61b7b5a1411fa
doubix <adib.grouz@gmail.com>
2020-03-31 17:21:03 +0100

    [update] user model mysql schema

--- models/user.py
+++ models/user.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
 """This is the user class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel):
     """This is the class for user
     Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
+        __tablename__ (str): MySQL table name to store users.
+        email: (sqlalchemy String): User's email address.
+        password (sqlalchemy String): User's password.
+        first_name (sqlalchemy String): User's first name.
+        last_name (sqlalchemy String): User's last name.
+        places (sqlalchemy relationship): The User-Place relationship.
+        reviews (sqlalchemy relationship): The User-Review relationship.
     """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")

fc29198087d5c8e2c8984c8b2f95ee65a2444a2e
doubix <adib.grouz@gmail.com>
2020-03-31 17:18:31 +0100

    [update] city model mysql schema

--- models/city.py
+++ models/city.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 """This is the city class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel):
     """This is the class for City
     Attributes:
-        state_id: The state id
-        name: input name
+        __tablename__ (str): MySQL table name to store Cities.
+        name (sqlalchemy String): City name.
+        state_id (sqlalchemy String): State id of the City.
     """
-    state_id = ""
-    name = ""
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship("Place", backref="cities", cascade="delete")

3925dccb49863986934ca526e10d9b5a396ef0a4
doubix <adib.grouz@gmail.com>
2020-03-31 17:19:49 +0100

    [update] review model mysql schema

--- models/review.py
+++ models/review.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 """This is the review class"""
-from models.base_model import BaseModel
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.orm import relationship
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """This is the class for Review
     Attributes:
-        place_id: place id
-        user_id: user id
-        text: review description
+        __tablename__ (str): MySQL table name to store Reviews.
+        text (sqlalchemy String): review description.
+        place_id (sqlalchemy String): review's place id.
+        user_id (sqlalchemy String): review's user id.
     """
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = "reviews"
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    text = Column(String(1024), nullable=False)
